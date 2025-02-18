933b8991ebb372e7768ead5aa094ae87d8c185fd
OHshajim <ajshajimmax@gmail.com>
2025-02-19 00:49:07 +0600

    project update

--- src/app/Login/[[...rest]]/page.tsx
+++ src/app/Login/[[...rest]]/page.tsx
@@ -1,3 +1,4 @@
+
 import { SignIn } from "@clerk/nextjs";
 
 const Login = () => {
--- src/app/api/protected/route.ts
+++ src/app/api/protected/route.ts
@@ -1,15 +1,1 @@
-// import  auth from "@clerk/nextjs";
-// import { NextResponse } from "next/server";
 
-// export async function GET() {
-//   const { userId } = auth();
-
-//   if (!userId) {
-//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-//   }
-
-//   return NextResponse.json({
-//     message: "Welcome! You are authenticated.",
-//     userId,
-//   });
-// }
